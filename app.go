3e23e377f96459e16058794bd063118aae72d4e9
Anthony Alves <aalves@fit.edu>
2017-10-05 09:20:17 -0400

    remme/app: refactor chpasswd to own function

--- app.go
+++ app.go
@@ -19,8 +19,9 @@ type App struct {
 }
 
 var (
-	emptyHost = fmt.Errorf("empty host")
-	hostDNE   = fmt.Errorf("host DNE")
+	emptyHost       = fmt.Errorf("empty host")
+	hostDNE         = fmt.Errorf("host DNE")
+	proxyParseError = fmt.Errorf("unable to change password")
 )
 
 func NewApp(datastore db.DataStore, services map[string]webservices.Webservice) *App {
@@ -56,14 +57,8 @@ func (a *App) ChangePasswords(out chan<- changer.Status, mngr managers.Manager, 
 	if err != nil {
 		log.Println("Unable to save log", err, lr)
 	}
-	out <- changer.Status{
-		JobId:    lr.JobID,
-		Type:     changer.Status_JOB_START,
-		Email:    mngr.GetEmail(),
-		Hostname: "",
-		Msg:      fmt.Sprintf("Total sites: %d", lr.TotalSites),
-		TaskId:   uint64(0),
-	}
+	out <- newStatus(lr.JobID, 0, changer.Status_JOB_START, mngr.GetEmail(), "",
+		fmt.Sprintf("Total sites: %d", lr.TotalSites))
 
 	taskId := uint64(0)
 	for _, site := range sites {
@@ -74,83 +69,16 @@ func (a *App) ChangePasswords(out chan<- changer.Status, mngr managers.Manager, 
 		service, err := a.searchService(site.Hostname)
 
 		taskId++
-		out <- changer.Status{
-			JobId:    lr.JobID,
-			TaskId:   taskId,
-			Type:     changer.Status_TASK_START,
-			Email:    site.Email,
-			Hostname: site.Hostname,
-			Msg:      "",
-		}
+		out <- newStatus(lr.JobID, taskId, changer.Status_TASK_START, site.Email, site.Hostname, "")
 
 		if err != nil {
 			lr.AddFailure(site.Hostname, site.Email, "Unsupported website", Version)
-			out <- changer.Status{
-				JobId:    lr.JobID,
-				TaskId:   taskId,
-				Type:     changer.Status_TASK_ERROR,
-				Email:    site.Email,
-				Hostname: site.Hostname,
-				Msg:      "Unsupported website",
-			}
+			out <- newStatus(lr.JobID, taskId, changer.Status_TASK_ERROR, site.Email, site.Hostname, "Unsupported website")
 			continue
 		}
 
 		wg.Add(1)
-		go func(goservice webservices.Webservice, gosite managers.Site, goTaskId uint64) {
-			lr.IncTries(1)
-			log.Println("Changing password for:", gosite.Hostname, gosite.Email)
-			newpasswd := gosite.Password //passwdFunc()) TODO
-			defer wg.Done()
-
-			err := goservice.ChangePassword(gosite.Email, gosite.Password, newpasswd)
-			if err != nil {
-				log.Println(err)
-				lr.AddFailure(gosite.Hostname, gosite.Email, err.Error(), Version)
-				out <- changer.Status{
-					JobId:    lr.JobID,
-					TaskId:   goTaskId,
-					Type:     changer.Status_TASK_ERROR,
-					Email:    gosite.Email,
-					Hostname: gosite.Hostname,
-					Msg:      err.Error(),
-				}
-				return
-			}
-
-			err = mngr.SavePassword(gosite.Hostname, gosite.Email, newpasswd)
-			if err != nil {
-				log.Printf("Failed to save password for %s %s.. reverting: %s\n", gosite.Hostname, gosite.Email, err)
-				lr.AddFailure(gosite.Hostname, gosite.Email, "Failed to save new password", Version)
-				err = goservice.ChangePassword(gosite.Email, newpasswd, gosite.Password)
-				if err != nil {
-					// oh shit boi.
-					log.Println(err)
-					lr.AddFailure(gosite.Hostname, gosite.Email, "Failed to revert back to old password", Version)
-					log.Printf("Failed to revert back to old password for %s %s.. %s\n", gosite.Hostname, gosite.Email, err)
-					// TODO send email to customer with new password?
-					out <- changer.Status{
-						JobId:    lr.JobID,
-						TaskId:   goTaskId,
-						Type:     changer.Status_TASK_ERROR,
-						Email:    gosite.Email,
-						Hostname: gosite.Hostname,
-						Msg:      "Failed to revert back to old password",
-					}
-					return
-				}
-			}
-
-			log.Println("Password changed for:", gosite.Hostname, gosite.Email)
-			out <- changer.Status{
-				JobId:    lr.JobID,
-				TaskId:   goTaskId,
-				Type:     changer.Status_TASK_FINISH,
-				Email:    gosite.Email,
-				Hostname: gosite.Hostname,
-				Msg:      "",
-			}
-		}(service, site, taskId)
+		go chPasswd(out, wg, service, mngr, site, lr, taskId)
 	}
 
 	wg.Wait()
@@ -159,15 +87,56 @@ func (a *App) ChangePasswords(out chan<- changer.Status, mngr managers.Manager, 
 	if err != nil {
 		log.Println("Unable to save log", err, lr)
 	}
-	out <- changer.Status{
-		JobId:    lr.JobID,
-		TaskId:   0,
-		Type:     changer.Status_JOB_FINISH,
-		Email:    mngr.GetEmail(),
-		Hostname: "",
-		Msg:      "",
-	}
+
+	out <- newStatus(lr.JobID, 0, changer.Status_JOB_FINISH, mngr.GetEmail(), "", "")
 	close(out)
+}
+
+func newStatus(jId, tId uint64, typ changer.Status_Type, email, hname, msg string) changer.Status {
+	return changer.Status{JobId: jId, TaskId: tId, Type: typ, Email: email, Hostname: hname, Msg: msg}
+}
+
+func chPasswd(out chan<- changer.Status, wg sync.WaitGroup, goservice webservices.Webservice,
+	mngr managers.Manager, gosite managers.Site, lr *db.LogRecord, goTaskId uint64) {
+
+	lr.IncTries(1)
+	log.Println("Changing password for:", gosite.Hostname, gosite.Email)
+	newpasswd := gosite.Password //passwdFunc()) TODO
+	defer wg.Done()
+
+	err := goservice.ChangePassword(gosite.Email, gosite.Password, newpasswd)
+	if err != nil {
+		log.Println(err)
+		lr.AddFailure(gosite.Hostname, gosite.Email, err.Error(), Version)
+		if _, ok := err.(webservices.ParseError); ok {
+			err = proxyParseError // user-friendly error
+		}
+
+		out <- newStatus(lr.JobID, goTaskId, changer.Status_TASK_ERROR, gosite.Email, gosite.Hostname, err.Error())
+		return
+	}
+
+	err = mngr.SavePassword(gosite.Hostname, gosite.Email, newpasswd)
+	if err != nil {
+		log.Printf("Failed to save password for %s %s.. reverting: %s\n", gosite.Hostname, gosite.Email, err)
+		lr.AddFailure(gosite.Hostname, gosite.Email, "Failed to save new password", Version)
+		err = goservice.ChangePassword(gosite.Email, newpasswd, gosite.Password)
+		if err != nil {
+			// oh shit boi. failed to revert password
+			log.Printf("Failed to revert back to old password for %s %s.. %s\n", gosite.Hostname, gosite.Email, err)
+			lr.AddFailure(gosite.Hostname, gosite.Email, "Failed to revert back to old password", Version)
+			if _, ok := err.(webservices.ParseError); ok {
+				err = proxyParseError // user-friendly error
+			}
+
+			// TODO send email to customer with new password?
+			out <- newStatus(lr.JobID, goTaskId, changer.Status_TASK_ERROR, gosite.Email, gosite.Hostname, "Failed to revert back to old password")
+			return
+		}
+	}
+
+	log.Println("Password changed for:", gosite.Hostname, gosite.Email)
+	out <- newStatus(lr.JobID, goTaskId, changer.Status_TASK_FINISH, gosite.Email, gosite.Hostname, "")
 }
 
 func (a *App) searchService(hostname string) (webservices.Webservice, error) {
